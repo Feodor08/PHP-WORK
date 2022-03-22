9bcb403c9b8dbb1a1403fe09f80b6333b97118c9
Feodor <fedyamedvedenko51@gmail.com>
2022-03-22 15:33:20 +0700

    Задача 8.2(от сафарова) php

--- code.php
+++ code.php
@@ -3,7 +3,7 @@ header('Content-Type: text/html; charset=utf-8');
 $x = $_POST['ch1'];
 $y = $_POST['ch2'];
 $z = $x + $y;
-echo $z;
+echo $z
 print_r($_POST);
 echo '<a href = "pip.html">Вернуться назад</a>';
 ?>
