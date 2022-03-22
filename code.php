15c01b6d2c828fddf83d8d89028ba0d84b421daa
Feodor <fedyamedvedenko51@gmail.com>
2022-03-22 15:30:06 +0700

    Задача 8.2(от сафарова) php

--- code.php
+++ code.php
@@ -1,6 +1,9 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
-$name = $_POST['n'];
-$surname = $_POST['s'];
-echo "Привет ".$name.' '.$surname;
+$x = $_POST['ch1'];
+$y = $_POST['ch2'];
+$z = $x + $y;
+echo $z;
+print_r($_POST);
+echo '<a href = "pip.html">Вернуться назад</a>';
 ?>
