f961992bce3f3ae8564ec2ae60797720c0faa4f4
Feodorgit config --global user.name Feodor <fedyamedvedenko51@gmail.com>
2022-03-29 15:21:36 +0700

    Задача 9.7 php

--- code.php
+++ code.php
@@ -1,5 +1,16 @@
 <?php 
-$a = file_get_contents('count.txt');
-echo $a+1;
-file_put_contents("count.txt",$a=$a+1);
+    header('Content-Type: text/html; charset utf-8');
+    $files = array('1.txt','2.txt','3.txt');
+    $str =''; 
+    
+    	echo $i;
+    }
+    for ($i=0; $i<count($files); $i++) {
+    for ($i=1; $i<count($files); $i++) {
+    	print_r($i);
+    	$name = $files[$i];
+    	echo '--'.$files[$i].'++<br>';
+    	$str .= file_get_contents($name);
+    }  
+    file_put_contents('new.txt', $str);
 ?>
