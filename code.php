6b266fc0b85c306d058026404be184d388206465
Feodorgit config --global user.name Feodor <fedyamedvedenko51@gmail.com>
2022-03-29 14:43:15 +0700

    Задача 9.5 php

--- code.php
+++ code.php
@@ -1,11 +1,5 @@
 <?php
     header('Content-Type: text/html; charset+utf-8');
-    $str1 = file_get_contents('1.txt');
-    $str2 = file_get_contents('2.txt');
-
-    $arr1 = explode(' ', $str1);
-    $arr2 = explode(' ', $str2);
-
-    $sum = array_sum($arr1) + array_sum($arr2);
-    file_put_contents('sum.txt', $sum);
+    $chislo = file_get_contents('3.txt');
+    file_put_contents('3.txt', $chislo*$chislo);
 ?>
