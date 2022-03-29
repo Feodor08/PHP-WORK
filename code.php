fe4b6ad932074d70888015ccd9e28a64f405ec5e
Feodorgit config --global user.name Feodor <fedyamedvedenko51@gmail.com>
2022-03-29 14:24:03 +0700

    Задача 9.1 php

--- code.php
+++ code.php
@@ -1,9 +1,14 @@
 <?php
-header('Content-Type: text/html; charset=utf-8');
-$x = $_POST['ch1'];
-$y = $_POST['ch2'];
-$z = $x + $y;
-echo $z
-print_r($_POST);
-echo '<a href = "pip.html">Вернуться назад</a>';
+    header('Content-Type: text/html; charset+utf-8');
+    $str1 = file_get_contents('1.txt');
+    $str2 = file_get_contents('2.txt');
+
+    $arr1 = explode(' ', $str1);
+    $arr2 = explode(' ', $str2);
+
+    $sum = 0;
+    for ($i=0; $i<count($arr1); $i++) {
+    	$sum += $arr1[$i];
+    }
+    echo $sum;
 ?>
