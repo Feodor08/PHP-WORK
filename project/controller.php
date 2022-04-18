67b77b76ef785d283224c2212023045b2cf685f6
Feodor <fedyamedvedenko51@gmail.com>
2022-04-18 15:09:02 +0700

    Задача 88.2

--- project/controller.php
+++ project/controller.php
@@ -1,17 +1,9 @@
 <?php
-	namespace Project;
-	use \Core\Users\Data;
-	class Page extends Controller
+	namespace Users;
+	use \Core\Admin\Controller
+	
+	class page extends controller
 	{
-	class Test
-	{
-		public function __construct()
-		{
-			// Создаем 3 объекта одного класса:
-			$data1  = new Data('user1');
-			$data2  = new Data('user2');
-			$data3  = new Data('user3');
-		}
+		
 	}
-}
 ?>
